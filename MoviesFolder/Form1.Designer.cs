b5450efab31d8c4d21cee3d97d9d74f65640b515
Gillions, Zac <zacharygillions@gmail.com>
2020-12-05 15:38:24 +1300

    Add project files.

---
+++ MoviesFolder/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MoviesFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appDesc = new System.Windows.Forms.Label();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.folderDirectory = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // appDesc
+            // 
+            this.appDesc.AutoSize = true;
+            this.appDesc.Location = new System.Drawing.Point(12, 9);
+            this.appDesc.Name = "appDesc";
+            this.appDesc.Size = new System.Drawing.Size(140, 13);
+            this.appDesc.TabIndex = 0;
+            this.appDesc.Text = "App for sorting movies folder";
+            this.appDesc.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point(15, 152);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(235, 251);
+            this.listBoxFiles.TabIndex = 1;
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFolder.Location = new System.Drawing.Point(87, 46);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(118, 20);
+            this.textBoxFolder.TabIndex = 2;
+            // 
+            // folderDirectory
+            // 
+            this.folderDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.folderDirectory.AutoSize = true;
+            this.folderDirectory.Location = new System.Drawing.Point(12, 49);
+            this.folderDirectory.Name = "folderDirectory";
+            this.folderDirectory.Size = new System.Drawing.Size(69, 13);
+            this.folderDirectory.TabIndex = 3;
+            this.folderDirectory.Text = "Select Folder";
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(211, 46);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(39, 20);
+            this.buttonGo.TabIndex = 4;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 415);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.folderDirectory);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.appDesc);
+            this.Name = "Form1";
+            this.Text = "Movies Folder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label appDesc;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Label folderDirectory;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonGo;
+    }
+}
+
---
+++ MoviesFolder/MovFolder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MoviesFolder
+{
+    public class MovFolder
+    {
+        private string dir;
+
+        public MovFolder(string dir)
+        {
+            this.dir = dir;
+        }
+
+        public void changeFolder(string dir) { this.dir = dir; }
+
+        public string[] getFiles()
+        {
+            if(Directory.Exists(dir))
+            {
+                return Directory.GetFiles(dir);
+            }
+            else
+            {
+                throw new IOException("Directory does not exist");
+            }
+        }
+    }
+}
